08784d103a3460c0e26765903c96cf7665a7cbb3
Pradyumn Shukla <50890905+pradyumn263@users.noreply.github.com>
2021-05-25 22:45:08 +0530

    Time: 160 ms (60.13%), Space: 65.8 MB (17.66%) - LeetHub

--- minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,63 +1,30 @@
 class Solution {
 public:
     
+    int numberOfBouquets(vector<int> &nums, int currentDay, int currentIndex, int flowersLeft, int flowersNeeded) {
+        if(currentIndex == nums.size() && flowersLeft == 0)
+            return 1;
+        
+        if(currentIndex == nums.size())
+            return 0;
+        
+        if(flowersLeft == 0) {
+            return 1 + numberOfBouquets(nums, currentDay, currentIndex, flowersNeeded, flowersNeeded);
+        }
+        
+        if (nums[currentIndex] <= currentDay) {
+            return numberOfBouquets(nums, currentDay, currentIndex + 1, flowersLeft - 1, flowersNeeded);
+        }
+        
+        if (nums[currentIndex] > currentDay) {
+            return numberOfBouquets(nums, currentDay, currentIndex + 1, flowersNeeded, flowersNeeded);
+        }
+        return 0;
+    }
+    
     bool canMake(vector<int> &nums, int day, int count, int windowSize) {
-        // Basically Sliding Maximum
-        int lo = 0;
-        int hi = 0;
-        
-        int res = 0;
-        
-        deque<int> dq;
-        
-        while (hi < windowSize) {
-            while(!dq.empty() && dq.back() < nums[hi])
-                dq.pop_back();
-            
-            dq.push_back(nums[hi]);
-            hi++;
-        }
-        
-        while(hi < nums.size()) {
-            if(dq.front() <= day) {
-                res++;
-                
-                while(!dq.empty())
-                    dq.pop_front();
-                
-                lo = hi;
-                 while (hi < lo + windowSize && hi < nums.size()) {
-                    while(!dq.empty() && dq.back() < nums[hi])
-                        dq.pop_back();
-
-                    dq.push_back(nums[hi]);
-                    hi++;
-                }
-                
-                if (hi - lo < windowSize) {
-                    return res >= count;
-                }
-                
-                continue;
-            }
-            
-            while(!dq.empty() && dq.back() < nums[hi])
-                dq.pop_back();
-            
-            if(!dq.empty() && dq.front() == nums[lo])
-                dq.pop_front();
-            
-            dq.push_back(nums[hi]);
-            lo++;
-            hi++;
-        }
-        
-        if(dq.front() <= day) {
-            res++;
-        }
-        
-        return res >= count;
-        
+        int res = numberOfBouquets(nums, day, 0, windowSize, windowSize);
+        return count <= res;
     }
     
     int minDays(vector<int>& bloomDay, int m, int k) {
