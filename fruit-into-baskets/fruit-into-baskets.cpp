2bf5880a9b31f83b77063ecf3c2c060e3144b816
Pradyumn Shukla <50890905+pradyumn263@users.noreply.github.com>
2021-05-28 00:00:17 +0530

    Time: 108 ms (68.75%), Space: 77.3 MB (5.31%) - LeetHub

--- fruit-into-baskets/fruit-into-baskets.cpp
+++ fruit-into-baskets/fruit-into-baskets.cpp
@@ -7,38 +7,20 @@ public:
         
         int lo = 0; int hi = 0;
         
-        while (hi < tree.size()) {
-            if (count[tree[hi]] == 0 && countDiff == 2)
-                break;
+        for (hi = 0; hi < tree.size(); hi++) {
+            count[tree[hi]]++;
+            if(count[tree[hi]] == 1) {
+                countDiff++;
+            }
             
-            if (count[tree[hi]] == 0) {
-                countDiff++;
-            }
-            count[tree[hi]]++;
-            hi++;
-        }
-        
-        res = hi;
-        
-        while (hi < tree.size()) {
-            if (count[tree[hi]] == 0) {
-                countDiff++;
-            }
-            count[tree[hi]]++;
-            hi++;
-            while(countDiff > 2) {
+            while (countDiff > 2) {
                 count[tree[lo]]--;
-                if (count[tree[lo]] == 0) {
+                if (count[tree[lo]] == 0)
                     countDiff--;
-                }
                 lo++;
             }
             
-            res = max(res, hi - lo);
-        }
-        
-        if (countDiff <= 2) {
-            res = max(res, hi - lo);
+            res = max(res, (hi - lo + 1));
         }
         return res;
     }
