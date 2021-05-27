30c98a7347ea4d5a92b05d6b47636e31264e2d21
Pradyumn Shukla <50890905+pradyumn263@users.noreply.github.com>
2021-05-27 19:15:10 +0530

    Time: 32 ms (60.45%), Space: 28.7 MB (54.16%) - LeetHub

--- binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -2,69 +2,43 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-       if (goal > nums.size())
-           return 0;
+       return atMost(nums, goal) - atMost(nums, goal-1);
+    }
+    
+    int atMost(vector<int> &nums, int sum) {
+        if (sum < 0)
+            return 0;
         
-        vector<pair<int,int>> zcount(nums.size(), {0,0});
-        int lastOneCount = nums.size()-1;
+        int lo = 0, hi = 0;
+        int res = 0, curr = 0;
         
-        for (int i = nums.size()-1; i >= 0; i--) {
-            if (nums[i] == 1) {
-                lastOneCount = i;
-                continue;
-            } else {
-                zcount[i].first = lastOneCount-i;
-                zcount[i].second = lastOneCount;
+        while(hi < nums.size()) {
+            if (nums[hi] + curr > sum)
+                break;
+            
+            curr+=nums[hi];
+            hi++;
+            
+            if (curr <= sum) {
+                res+=hi;
             }
         }
         
-        int res = 0;
-        
-        int hi = 0, lo = 0;
-        int count = 0;
-        
-        while(hi < nums.size() && count <= goal) {
-            
-            if (nums[hi])
-                count++;
-            hi++;
-            if(count == goal)
-                break;
-        }
-        
-        if(count < goal)
-            return 0;
-        if(count == goal && zcount[lo].second == zcount[hi-1].second) {
-            res += 1 + (zcount[lo].first - zcount[hi-1].first);
-        } else if(count == goal && zcount[lo].second < hi - 1) {
-            res+= 1 + zcount[lo].first;
-        } else if (count == goal) {
-            res++;
-        }
-        while(hi < nums.size()) {
-            if(nums[hi])
-                count++;
+        while (hi < nums.size()) {
+            curr += nums[hi];
             hi++;
             
-            while(lo < hi && count > goal) {
-                if(nums[lo])
-                    count--;
+            while(curr > sum) {
+                curr -= nums[lo];
                 lo++;
             }
-            if (lo == hi)
-                continue;
-            if(lo < nums.size() && zcount[lo].second == zcount[hi-1].second) {
-                res += 1 + zcount[lo].first - zcount[hi-1].first;
-            } else if(lo < nums.size() && zcount[lo].second < hi - 1) {
-                res+=1 + zcount[lo].first;
-            } else {
-                if (hi < nums.size() && lo < nums.size() && count == goal)
-                    res++;
+            if (curr <= sum) {
+                res+= (hi - lo);
             }
-            cout << "HI: " << hi << " LO: " << lo << " RES: " << res << "\n";
         }
         
         return res;
-        
     }
+    
+    
 };
