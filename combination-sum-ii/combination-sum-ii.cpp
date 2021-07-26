f1b314e7e8059f64b170fe5de26117da71c3cd37
Pradyumn Shukla <50890905+pradyumn263@users.noreply.github.com>
2021-07-26 18:10:57 +0530

    Time: 4 ms (92.70%), Space: 10.6 MB (64.79%) - LeetHub

--- combination-sum-ii/combination-sum-ii.cpp
+++ combination-sum-ii/combination-sum-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Check Commit history for another implementation
+    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> res;
         vector<int> currset;
@@ -9,27 +11,20 @@ public:
     }
     
     void combinationHelper(vector<vector<int>> &res, vector<int> &nums, vector<int> &currset, int index, int target) {
-        if (index == nums.size()) {
-            if (target == 0)
-                res.push_back(currset);
-            return;
-        }
-        
         if (target == 0) {
             res.push_back(currset);
             return;
         }
         
-        if (target < 0)
+        if (target < 0 || index >= nums.size())
             return;
-        int tempIndex = index;
-        while (tempIndex < nums.size() && nums[index] == nums[tempIndex])
-            tempIndex++;
-        combinationHelper(res, nums, currset, tempIndex, target);
         
-        currset.push_back(nums[index]);
-        combinationHelper(res, nums, currset, index+1, target - nums[index]);
-        currset.pop_back();
-        return;
+        for (int i = index; i < nums.size(); i++) {
+            if (i != index && nums[i] == nums[i-1]) continue;
+            
+            currset.push_back(nums[i]);
+            combinationHelper(res, nums, currset, i+1, target-nums[i]);
+            currset.pop_back();
+        }
     }
 };
